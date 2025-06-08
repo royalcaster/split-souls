fd4c929a5c412c580bbad6d5e15b980ea5b9d4b2
Christin Flischikowski <s87513@stud.htw-dresden.de>
2025-06-08 19:44:38 +0200

    minor update bugfix

--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -13,7 +13,7 @@ func _on_area_2d_body_entered(body):
 	var game = get_parent().get_parent()
 	if game.has_method("on_crystal_direction_item_collected"):
 		game.rpc("on_crystal_direction_item_collected")
-	await get_tree().idle_frame
+#	await get_tree().idle_frame
 
 	queue_free()
 	$"../../AudioManager".play_audio_2d("item01")
